e8055200fff6c2bfb2956b3fda53ad49696655ce
jonghopark95 <jong951005@gmail.com>
2021-07-06 11:30:37 +0900

    chore: Connect with view templates

--- server/index.js
+++ server/index.js
@@ -1,10 +1,15 @@
 const express = require("express");
+const pug = require("pug");
+const path = require("path");
 
 const app = express();
 const PORT = 3000;
 
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
+
 app.get("/", (req, res) => {
-  res.send("hihi");
+  res.render("index", { title: "getget", message: "testrtest" });
 });
 
 app.listen(PORT, () => {
