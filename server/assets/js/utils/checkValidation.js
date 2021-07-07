b34a07f3bf0de8c575b869e5d460128dce477b08
jonghopark95 <jong951005@gmail.com>
2021-07-08 01:41:39 +0900

    chore: Add regex logic

---
+++ server/assets/js/utils/checkValidation.js
@@ -0,0 +1,18 @@
+export const checkEmailValidation = (email) => {
+  return EMAIL_REGEX.test(email);
+};
+
+export const checkPwValidation = (pw) => {
+  const isPassedRuleOne = PW_REGEX_RULE_ONE.test(pw);
+  const isPassedRuleTwo = !PW_REGEX_RULE_TWO.test(pw);
+
+  return isPassedRuleOne && isPassedRuleTwo;
+};
+
+const EMAIL_REGEX = /^\S+@\S+\.(\S{2,})+/;
+
+const PW_REGEX_RULE_ONE =
+  /^(?=.*[A-Z])(?=.*[a-z])([^\s]){10,}|(?=.*[A-Z])(?=.*[0-9])([^\s]){10,}|(?=.*[A-Z])(?=.*[<>{}|;:.,~!?@#$%^=&*\”\\/])([^\s]){10,}|(?=.*[a-z])(?=.*[0-9])([^\s]){10,}|(?=.*[a-z])(?=.*[<>{}|;:.,~!?@#$%^=&*\”\\/])([^\s]){10,}|(?=.*[0-9])(?=.*[<>{}|;:.,~!?@#$%^=&*\”\\/])([^\s]){10,}$/;
+
+const PW_REGEX_RULE_TWO =
+  /(012|123|234|345|456|567|678|789|987|876|765|654|543|432|321|210|([0-9]{3,}))+/g;
