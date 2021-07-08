7c1a1684cd118cafead5031f34fa0db0ddb0b754
jonghopark95 <jong951005@gmail.com>
2021-07-09 01:02:17 +0900

    refactor: Refactor homeRouter.js

--- server/routes/homeRouter.js
+++ server/routes/homeRouter.js
@@ -1,12 +1,15 @@
-const express = require('express');
+const express = require("express");
 const homeRouter = express.Router();
 
+const PAGE_TITLE = "대한민국 1등 배달앱, 배달의민족";
+
+/**
+ * 로그인을 해서 세션이 남아있는 경우 user 정보를,
+ * 그렇지 않다면 null을 template에 넣어줍니다.
+ */
 homeRouter.get("/", (req, res) => {
-  if(req.session?.user) { // 로그인 했을 때
-    res.render("index", { title: "배민 로그인", user: req.session.user });
-  } else { //로그인이 아닐 때
-    res.render("index", { title: "배민 로그인", user: null });
-  }
+  const userInfo = req.session?.user ? req.session.user : null;
+  res.render("index", { title: PAGE_TITLE, user: userInfo });
 });
 
 module.exports = homeRouter;
